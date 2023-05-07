e24aa5e33a95c73ae178595d0135b8b0c842b0cb
Raj Patel <65962874+bonumati@users.noreply.github.com>
2023-05-07 16:54:20 -0400

    Delete Rental-Service-BackEnd-master directory

--- Rental-Service-BackEnd-master/src/routes/admin.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as adminData from "../data/admin";
-const routes = Router();
-
-routes.get("/users", adminData.getUsers);
-routes.get("/properties", adminData.getProperties);
-routes.get("/payments", adminData.getPayments);
-routes.get("/bookings", adminData.getBookings);
-export default routes;
